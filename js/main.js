75405cfec27893b4e016c84f98f4df5c1e70dcfb
Mahmudul Hassan <mahmudul.coder@gmail.com>
2024-10-31 22:54:15 +0600

    Form connected to Hubspot

--- js/main.js
+++ js/main.js
@@ -45,4 +45,48 @@
       });
     }, 3000); // Change word every 3 seconds
   })();
+
+  $('.case_description_form').on('submit', function(event){
+    event.preventDefault();
+    $('#chatModal').modal('show');
+    const formData = $(this).serializeArray();
+    const fields = formData.map(field => ({
+      name: field.name,
+      value: field.value
+    }));
+    $('#case_description').val(fields[0].value)
+
+    // Reset the form fields after submission
+    $('.case_description_form').trigger("reset");
+  });
+
+  $('.chat-form').on('submit', function(event) {
+    event.preventDefault();
+
+    const formData = $(this).serializeArray();
+    const fields = formData.map(field => ({
+      name: field.name,
+      value: field.value
+    }));
+
+    $.ajax({
+      url: 'https://api.hsforms.com/submissions/v3/integration/submit/47511090/1c7ae950-e4d0-45f8-96c0-21afe0c880e1',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ fields: fields }),
+      success: function(response) {
+        // Close the modal
+        $('#chatModal').modal('hide');
+        // Reset the form fields after submission
+        $('.chat-form').trigger("reset");
+        // Show the success modal
+        $('#thankModal').modal('show');        
+      },
+      error: function(xhr, status, error) {
+        console.error("Error:", status, error);
+        alert("Form submission failed. Please try again.");
+      }
+    });
+  });
+
 })(jQuery);
